2066a80b8eb527db4d5a9eae1f99ca61e9e6a117
andresCasas <felipecasascastro@gmail.com>
2026-09-17 17:15:07 -0500

    chore:se suben cambios en el backend en index.js y db.js

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,15 @@
 import mysql from 'mysql2/promise';
 
-// se crea una function para aceptar las conexiones simultanias
+// Configuración de la conexión a MySQL usando las variables del archivo .env
+const poll = mysql.createPool({
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'gae',
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-const pool = mysql.createPool ({
-    host: process.env.DB_host,  // direccion del servidor ejemplo: localhost o ip del  servidor de la nube
-    user: process.env.DB_user, // Usuario de la base de datos
-    password: process.env.DB_password, // password de la base de datos
-    database: process.env.DB_name,  // Nombre de la base de datos
-    waitForConnections: true, // tiempo de espera para dar a un usuario cuando una conxion es libre
-    connectionLimit: 10, // cantidad de usuarios que voy a dejar conectar, en este caso 10 usuarios
-    queueLimit: 0 // se define cuantas peteiciones se pueden quedar esperando en la fila
-    
-})
-
-export default pool; 
+export default poll;
